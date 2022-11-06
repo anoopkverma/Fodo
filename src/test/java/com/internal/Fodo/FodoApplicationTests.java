18f66f835fa9d6fa04086320d22b2e76d013346d
anoopkverma <anoopvermastp@outlook.com>
2022-11-06 15:49:56 +0530

    Initial commit

---
+++ src/test/java/com/internal/Fodo/FodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.internal.Fodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

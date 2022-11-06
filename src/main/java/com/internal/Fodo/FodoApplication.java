526cb87a192eb838c683dacfc575848b3b7d401b
anoopkverma <anoopvermastp@outlook.com>
2022-11-06 15:49:56 +0530

    Initial commit

---
+++ src/main/java/com/internal/Fodo/FodoApplication.java
@@ -0,0 +1,13 @@
+package com.internal.Fodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FodoApplication.class, args);
+	}
+
+}

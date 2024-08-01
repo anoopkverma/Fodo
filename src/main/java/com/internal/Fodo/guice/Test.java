d03c8de25847e6f8376e2c0c3323e7ba6c9c3ad8
anoopkverma <anoopvermastp@outlook.com>
2024-08-01 14:29:10 +0530

    Update

---
+++ src/main/java/com/internal/Fodo/guice/Test.java
@@ -0,0 +1,13 @@
+package com.internal.Fodo.guice;
+
+import com.google.inject.Inject;
+
+public class Test {
+
+    @Inject
+    static Sender sender;
+
+    public static void main(String[] args) {
+        sender.send();
+    }
+}

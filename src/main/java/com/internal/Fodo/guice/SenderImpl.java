608cf8ea9c958af9f6e6994a5c1f9c385e1b6109
anoopkverma <anoopvermastp@outlook.com>
2024-08-01 13:51:12 +0530

    Add guice file

---
+++ src/main/java/com/internal/Fodo/guice/SenderImpl.java
@@ -0,0 +1,9 @@
+package com.internal.Fodo.guice;
+
+public class SenderImpl implements Sender {
+
+    @Override
+    public void send() {
+            System.out.println("message is sent");
+    }
+}
